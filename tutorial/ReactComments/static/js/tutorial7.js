055e4cf23895d0b5c58f766ff152d5447baef2f5
Xitij Ritesh Patel <xpatel@pulsecode.ca>
2014-07-08 17:17:44 -0400

    Updated tutorial to 7th step, to circumvent React's XSS protection to allow HTML output.

--- tutorial/ReactComments/static/js/tutorial7.js
+++ tutorial/ReactComments/static/js/tutorial7.js
@@ -1,15 +1,16 @@
 /** @jsx React.DOM */
-// tutorial5.js
+// tutorial7.js
 
 var converter = new Showdown.converter();
 var Comment = React.createClass({
     render: function() {
+        var rawMarkup = converter.makeHtml(this.props.children.toString());
         return (
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {converter.makeHtml(this.props.children.toString())}
+                <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
             </div>
         );
     }
