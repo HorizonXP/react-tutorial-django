7c7c577f2f1010dffe47255c59b721bab329a9c9
Xitij Ritesh Patel <xpatel@pulsecode.ca>
2014-07-08 17:12:03 -0400

    Updated ReactComments to fifth step of tutorial with additional Comment React component.

--- tutorial/ReactComments/static/js/tutorial5.js
+++ tutorial/ReactComments/static/js/tutorial5.js
@@ -1,11 +1,25 @@
 /** @jsx React.DOM */
-// tutorial3.js
+// tutorial5.js
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
 
 var CommentList = React.createClass({
     render: function() {
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
+                <Comment author="Pete Hunt">This is one comment.</Comment>
+                <Comment author="Jordan Walke">This is *another* comment.</Comment>
             </div>
         );
     }
